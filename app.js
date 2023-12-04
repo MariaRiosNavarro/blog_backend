69065575473667c61cfd966522f29be5c59467fd
SuperCoder <propscode254@gmail.com>
2023-12-04 16:31:00 +0100

    add app.js and routes

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import {
+  setup,
+  saveArticle,
+  getAllArticle,
+  deleteArticle,
+} from "./utils/filestorage.js";
+
+//config
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 const app = express();
+
+//use
 
 app.use(express.json());
 
@@ -14,3 +24,36 @@ app.use(
     origin: process.env.CORS_ORIGIN,
   })
 );
+
+// Routes
+
+//  GET Route
+
+app.get("/api/articles", (req, res) => {
+  getAllArticle()
+    .then((data) => res.json(data))
+    .catch(() => res.status(500).end());
+});
+
+//  POST Route
+
+app.post("/api/articles", (req, res) => {
+  const article = req.body;
+  console.log("Article :", article);
+  saveArticle(article);
+  res.end();
+});
+
+//  Delete Route
+
+app.delete("api/articles", (req, res) => {
+  const id = req.body.id;
+  deleteArticle(id)
+    .then(() => res.end())
+    .catch((err) => res.status(500).end(err));
+});
+
+// Listen
+app.listen(PORT, () => {
+  console.log("Port is:http://localhost:" + PORT);
+});
---
+++ utils/filestorage.js
@@ -0,0 +1,45 @@
+import fs from "fs/promises";
+import fsystem from "fs";
+import { v4 as uuidv4 } from "uuid";
+
+//!create storage folder if dont exist
+
+export const setup = () => {
+  fs.access("./storage/")
+    .then(() => console.log("Storage Folder allready exist"))
+    .catch(() => fs.mkdir("/storage"));
+};
+
+//!save a new Blog
+
+export const saveArticle = (
+  article = {
+    title: "title",
+    description: "description",
+    tags: [],
+    favorite: false,
+  }
+) => {
+  article.id = uuidv4();
+  fs.writeFile("./storage/" + article.id, JSON.stringify(character));
+};
+
+// !create one Array with all Articles
+
+export const getAllArticle = () => {
+  return fs.readdir("./storage").then((files) => {
+    const array = [];
+    for (const file of files) {
+      const data = fsystem.readFileSync("./storage");
+      const char = JSON.parse(data);
+      array.push(char);
+    }
+    return array;
+  });
+};
+
+// ! delete Blog
+
+export const deleteArticle = (id) => {
+  return fs.rm("/storage/" + id);
+};
